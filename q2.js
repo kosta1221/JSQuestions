544a69cb663e84137e2bf0fb6965bc85c4a7d380
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-01-18 15:18:15 +0200

    Second question

---
+++ q2.js
@@ -0,0 +1,5 @@
+let num1, num2, num3;
+num1 = prompt("Input the first number", "0");
+num2 = prompt("Input the second number", "0");
+num3 = prompt("Input the third number", "0");
+num1 * num2 * num3 < 0 ? alert("The sign is -") : alert("The sign is +");
