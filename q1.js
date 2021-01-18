5bb030730948d0584c29afef5a214b5987c41352
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-01-18 13:29:08 +0200

    First question

--- q1.js
+++ q1.js
@@ -1,4 +1,4 @@
-let myVar = (x) => {
-	return x + 2;
-};
-console.log(typeof myVar);
+let int1, int2;
+int1 = prompt("Input the First integer", "0");
+int2 = prompt("Input the second integer", "0");
+int1 > int2 ? alert("The bigger number is " + int1) : alert("The bigger number is " + int2);
